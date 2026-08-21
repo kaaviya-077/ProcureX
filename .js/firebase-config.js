7141f56df8c4d3905f67ed356509d451413583bc
kaaviya-077 <kaaviyaraju7@gmail.com>
2026-08-21 15:17:49 +0530

    Add files via upload

---
+++ .js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCphmi6H8D3eQw6K1rGAl-OGj3e50_zdOw',
+  authDomain: 'ai-powered-procurement-erp.firebaseapp.com',
+  projectId: 'ai-powered-procurement-erp',
+  storageBucket: 'ai-powered-procurement-erp.firebasestorage.app',
+  messagingSenderId: '360738872663',
+  appId: '1:360738872663:web:94b8d8110c4b208986f212',
+  measurementId: 'G-84JJMTQL6V'
+};
